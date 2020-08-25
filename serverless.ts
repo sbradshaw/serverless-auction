a22555a84d3d4bf6049683c6ccf8ecf292c484b9
Stuart Bradshaw <stuartb@Hyperion.local>
2020-08-25 16:54:49 +0100

    Initial commit

---
+++ serverless.ts
@@ -0,0 +1,43 @@
+import { Serverless } from "serverless/aws";
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: "serverless-auction",
+  },
+  frameworkVersion: ">=1.72.0",
+  custom: {
+    webpack: {
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
+  },
+  plugins: ["serverless-webpack"],
+  provider: {
+    name: "aws",
+    runtime: "nodejs12.x",
+    memorySize: 256,
+    region: "eu-west-1",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+    },
+    stage: "${opt: stage, 'dev'}",
+  },
+  functions: {
+    hello: {
+      handler: "handler.hello",
+      events: [
+        {
+          http: {
+            method: "get",
+            path: "hello",
+          },
+        },
+      ],
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
